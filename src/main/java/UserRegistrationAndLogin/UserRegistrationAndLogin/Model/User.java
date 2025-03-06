a91afb606c97d0a69c043970feec051ae604ed93
Krish-log <gopi.maganti1998@gmail.com>
2025-03-06 18:07:42 -0500

    added password hashings to the code

--- src/main/java/UserRegistrationAndLogin/UserRegistrationAndLogin/Model/User.java
+++ src/main/java/UserRegistrationAndLogin/UserRegistrationAndLogin/Model/User.java
@@ -7,6 +7,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "users")
@@ -22,18 +25,28 @@ public class User {
     @Column(name = "user_id")
     private Long id;
 
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
     @Column(nullable = false, unique = true, name = "username")
     private String username;
 
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 120, message = "Password must be between 6 and 120 characters")
     @Column(nullable = false, name = "password")
     private String password;
 
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be a valid email address")
     @Column(nullable = false, unique = true, name = "email")
     private String email;
 
+    @NotBlank(message = "First name is required")
+    @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters")
     @Column(nullable = false, name = "first_name")
     private String firstName;
 
+    @NotBlank(message = "Last name is required")
+    @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters")
     @Column(nullable = false, name = "last_name")
     private String lastName;
 }
