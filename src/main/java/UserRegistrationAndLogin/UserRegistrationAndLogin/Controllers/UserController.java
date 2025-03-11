3cd5b03170e310d260fa2707656a5d4c992ecc20
Krish-log <gopi.maganti1998@gmail.com>
2025-03-11 11:59:18 -0400

    Started adding Frontend

--- src/main/java/UserRegistrationAndLogin/UserRegistrationAndLogin/Controllers/UserController.java
+++ src/main/java/UserRegistrationAndLogin/UserRegistrationAndLogin/Controllers/UserController.java
@@ -8,16 +8,16 @@ import UserRegistrationAndLogin.UserRegistrationAndLogin.Repositories.UserReposi
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import jakarta.validation.Valid;
 
-@Controller
+@RestController
 @RequestMapping("/api/users")
 @CrossOrigin(origins = "*")
 @Validated
